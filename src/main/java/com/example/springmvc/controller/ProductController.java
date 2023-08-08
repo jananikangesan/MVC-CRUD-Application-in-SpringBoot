57a8089bdb392f8a60c47b67981cb1d26c1342af
jananikangesan <janakanges1996@gmail.com>
2023-08-08 16:05:05 +0530

    Create Product Page

--- src/main/java/com/example/springmvc/controller/ProductController.java
+++ src/main/java/com/example/springmvc/controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.example.springmvc.controller;
 
+import com.example.springmvc.model.Product;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ProductController {
@@ -9,5 +12,10 @@ public class ProductController {
     @RequestMapping(path="/")
     public String index(){
         return "index";
+    }
+
+    @RequestMapping(path="/products/add" , method= RequestMethod.GET)
+    public String CreateProduct(Model model){
+        model.addAttribute("product",new Product());
     }
 }
